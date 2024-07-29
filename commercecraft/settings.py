36d3d1ec32819275e1d8f797109cd72111a94466
SYN606 <postbox.501@yandex.com>
2024-07-29 22:54:17 +0530

    Setup static files and templates

--- commercecraft/settings.py
+++ commercecraft/settings.py
@@ -99,11 +99,20 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = 'static/'
 
-STATIC_URL = "static/"
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static"
+    ]
+else:
+    STATIC_ROOT = BASE_DIR / "static"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# media root
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
